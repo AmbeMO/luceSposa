67c2ad3197f23051adf3ad436e991891df8155ef
AmbeMO <milamo@mail.ua>
2021-08-26 16:22:28 +0300

    toggling forms

--- project/js/script.js
+++ project/js/script.js
@@ -7,4 +7,34 @@ $(document).ready(function(){
 		autoplaySpeed: 3000,
 
 	});
+
+	$(function(){
+		$(function() {
+        var salon = $('#form-salon'),
+            bride = $('#form-bride'),
+            feedback = $('#form-feedback'),
+            buttonsalons = $('#salons'),
+            buttonbrides = $('#brides');
+            buttonfeedbacks = $('#feedbacks');
+
+        buttonsalons.click(function() {
+            salon.show();
+            bride.hide();
+            feedback.hide();
+        });
+
+       buttonbrides.click(function() {
+            salon.hide();
+            bride.show();
+            feedback.hide();
+        });
+        buttonfeedbacks.click(function() {
+            salon.hide();
+            bride.hide();
+            feedback.show();
+        });
+    });
+
+})(jQuery);
+
 });
